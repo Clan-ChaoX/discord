b0198e15af3ee86e64e292e6bfda5f967332abad
DavFount <47730893+DavFount@users.noreply.github.com>
2022-10-02 00:55:04 -0700

    Added /register command

--- commands/register.js
+++ commands/register.js
@@ -1,20 +1,54 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { api } = require("../services/api");
+const { Config } = require("../config.json");
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${Config.api.auth_token}`,
+  },
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("register")
-    .setDescription("Register as a runner for Clan ChX.")
-    .addStringOption((option) =>
-      option
-        .setName("password")
-        .setDescription("Password used access CRU and Website.")
-        .setRequired(true)
-    ),
+    .setDescription("Register as a runner for Clan ChX."),
   async execute(interaction) {
-    // Register user as a runner -> Input string not required
-    await interaction.reply({
-      content: interaction.options.getString("password"),
-      ephemeral: true,
-    });
+    const user_id = interaction.user.id;
+    api
+      .get(`runners/${user_id}/`, config)
+      .then(async (result) => {
+        if (result.data.status === 200) {
+          await interaction.reply({
+            content: `Discord ID: ${user_id}\nChaoX ID: ${result.data.result.chaox_id}`,
+            ephemeral: true,
+          });
+        }
+      })
+      .catch(async (error) => {
+        if (error.response.status === 404) {
+          api
+            .post(`runners/${user_id}/`, {}, config)
+            .then(async (result) => {
+              await interaction.reply({
+                content: `Discord ID: ${user_id}\nChaoX ID: ${result.data.result.chaox_id}`,
+                ephemeral: true,
+              });
+            })
+            .catch(async (error) => {
+              console.log(error);
+              await interaction.reply({
+                content: `SumTingWong`,
+                ephemeral: true,
+              });
+            });
+        } else {
+          console.log(error.response.status);
+          await interaction.reply({
+            content: `SumTingWong`,
+            ephemeral: true,
+          });
+        }
+      });
   },
 };
